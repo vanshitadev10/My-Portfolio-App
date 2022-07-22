607892b8def3bd633f04ad30d3360c819308b7e1
Vanshita Dev <vanshita010@gmail.com>
2022-07-22 17:18:33 +0530

    minor changes

--- src/components/Introduction/IntroSection.js
+++ src/components/Introduction/IntroSection.js
@@ -37,7 +37,7 @@ const IntroSection = () => {
                 <p style={{ fontWeight: !modeCtx.mode ? 'bold' : 'normal' }}>I am a Frontend Web Developer with experience of building web applications with React.js {/* / Next.js  */}/ Javascript and some other cool libraries, custom plugins, animations and frameworks.</p>
                 <IconButtons inverse />
                 <div className={styles.intro__btn}>
-                    <Button id='btn4' onClick={() => setLink('https://drive.google.com/file/d/1am57gPbfy8QGlpaAvgtNK0jnNPSlCFBi/view?usp=sharing')}>SEE MY RESUME</Button>
+                    <Button id='btn4' onClick={() => setLink('https://drive.google.com/file/d/1am57gPbfy8QGlpaAvgtNK0jnNPSlCFBi/view?usp=sharing')}>MY RESUME</Button>
                     <a href='#contact' alt='Contact Me'><Button id='btn5'>CONTACT ME</Button></a>
                 </div>
             </aside>
